242e37d966a7a453e7d33892d581dcbc6e44c539
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-03 15:46:17 -0600

    might need user api in the future to delete a user or get a user

---
+++ src/main/java/onlinetutoring/com/teamelevenbackend/api/UserApis.java
@@ -0,0 +1,12 @@
+package onlinetutoring.com.teamelevenbackend.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/users")
+public class UserApis {
+
+}
