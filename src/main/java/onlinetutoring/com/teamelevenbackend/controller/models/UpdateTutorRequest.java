d737672d928adae297b5e0aeee7623fe3a20a4c2
Abigail Solomon <abituyee@gmail.com>
2023-03-07 18:48:51 -0600

    Update

--- src/main/java/onlinetutoring/com/teamelevenbackend/controller/models/UpdateTutorRequest.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/controller/models/UpdateTutorRequest.java
@@ -1,44 +1,9 @@
 package onlinetutoring.com.teamelevenbackend.controller.models;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.List;
 
-public class UpdateTutorRequest implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    private String fName;
-    private String lName;
-    private String email;
-    private String password;
-    private String profilePic;
-    private String aboutMe;
+public class UpdateTutorRequest extends AbstractUpdateRequest {
     private List<String> subjects;
-
-    public String getfName() {
-        return fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public String getAboutMe() {
-        return aboutMe;
-    }
-
     public List<String> getSubjects() {
         return subjects;
     }
