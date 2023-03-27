9c23799832508efdc0b6ee3d8b1569383038cc85
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-26 21:52:22 -0500

    update cors

--- src/main/java/onlinetutoring/com/teamelevenbackend/controller/AuthController.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
+@CrossOrigin
 @RequestMapping("/auth")
 public class AuthController {
 
