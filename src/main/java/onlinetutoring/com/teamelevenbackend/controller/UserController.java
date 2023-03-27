4fa7a745195a15e205a7399c16a9cee7972726ec
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-26 23:05:53 -0500

    cors attempt two

--- src/main/java/onlinetutoring/com/teamelevenbackend/controller/UserController.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController {
     private UserService userService;
     @Autowired
