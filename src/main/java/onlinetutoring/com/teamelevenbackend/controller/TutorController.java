3b732f94630846f365116baf6c42e30abebc95ce
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-26 22:07:36 -0500

    Revert "update cors"

--- src/main/java/onlinetutoring/com/teamelevenbackend/controller/TutorController.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/controller/TutorController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,6 @@ import java.util.List;
 
 @Controller
 @RestController
-@CrossOrigin
 @RequestMapping("/tutors")
 public class TutorController {
 
