00d4bb019f83435f71ca8fcccf5067ae19c6f603
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-28 19:27:36 -0500

    test cors 2

--- src/main/java/onlinetutoring/com/teamelevenbackend/controller/ControllerUtils.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/controller/ControllerUtils.java
@@ -1,6 +1,6 @@
 package onlinetutoring.com.teamelevenbackend.controller;
 
 public final class ControllerUtils {
-    public static final String BASE_PRODUCTION = "https://online-tutoring-backend.up.railway.app";
-    public static final String BASE_LOCAL = "http://localhost:8080";
+    public static final String BASE_PRODUCTION = "";
+    public static final String BASE_LOCAL = "http://localhost:3000";
 }
