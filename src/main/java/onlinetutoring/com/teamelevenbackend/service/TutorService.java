e63c862bab1f79471ea5151708c1f7e28c454fc8
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-03-29 21:05:30 -0500

    fix: hours

--- src/main/java/onlinetutoring/com/teamelevenbackend/service/TutorService.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/service/TutorService.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -208,6 +209,26 @@ public class TutorService {
                         .and(AVAILABLE_HOURS.DAY_OF_WEEK.eq(modifyAvailableHours.getDayOfWeek().toString()))
                         .execute();
             } else {
+                final LocalTime endTime = modifyAvailableHours.getEndTime();
+                final LocalTime startTime = modifyAvailableHours.getStartTime();
+
+                for (AvailableHoursRecord avHrsRec : availableHoursRecord) {
+                    // if current end-times is greater than to-set-end-time,
+                    // the current start-time should be greater than or equal to the to-set-end-time
+                    if (avHrsRec.getEndTime().isAfter(endTime)) {
+                        if (avHrsRec.getStartTime().isBefore(endTime)) {
+                            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                        }
+                    }
+                    // if current end-time is smaller than to-set-end-time,
+                    // the current end-time should also be
+                    else {
+                        if (startTime.isBefore(avHrsRec.getEndTime())) {
+                            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                        }
+                    }
+                }
+
                 // insert into table
                 dslContext.insertInto(AVAILABLE_HOURS)
                         .set(AVAILABLE_HOURS.TUTOR_ID, user.getId())
