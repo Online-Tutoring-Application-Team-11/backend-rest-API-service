53fa0a2a549ad0eb33ef8b1d4bd4657160e493ae
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2023-04-29 01:08:51 -0500

    test_combined tutor

--- src/main/java/onlinetutoring/com/teamelevenbackend/models/TutorUser.java
+++ src/main/java/onlinetutoring/com/teamelevenbackend/models/TutorUser.java
@@ -1,13 +1,13 @@
 package onlinetutoring.com.teamelevenbackend.models;
+import lombok.Getter;
+import lombok.Setter;
+import onlinetutoring.com.teamelevenbackend.entity.tables.pojos.AvailableHours;
 import onlinetutoring.com.teamelevenbackend.entity.tables.pojos.Users;
 import java.util.List;
 
+@Getter
+@Setter
 public class TutorUser extends Users {
     private List<String> subjects;
-
-    public List<String> getSubjects(){ return subjects; }
-
-    public void setSubjects(List<String> subjects) {
-        this.subjects = subjects;
-    }
+    private List<AvailableHours> availableHours;
 }
